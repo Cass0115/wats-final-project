5705e03291ddbb069ab0d299994b1776d2aa3e73
Cassie Durr <Cass0115@gmail.com>
2017-06-01 23:14:51 -0700

    Updated some styling, added query validation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 // Javascript file
+$(function(){
+  $('div.form-group').prop('required',true);
+    $.validator.addMethod('withSpaces', function(value, element){
+      return value.match(/^[a-zA-Z]+$/);
+
+    }, "Please use letters and spaces only."
+  );
+
+  $('order-form').validate({
+    submitHandler: function(form){
+      form.submit();
+    },
+
+    rules:{
+      'your-name':{
+        required:true,
+        maxlength:100,
+        withSpaces:true
+      },
+
+    } // rules
+    })// order-form
+}); //document function
